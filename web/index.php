b966b226389f60f059e8b21beeb986529b65d0ae
Marco Fischer <mfischer@brainbits.net>
2015-08-05 17:20:09 +0200

    fixed

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ define('CONFIGDIR', APPDIR . DIRECTORY_SEPARATOR . 'config');
 
 $config = json_decode(file_get_contents(CONFIGDIR . DIRECTORY_SEPARATOR . 'config.json'));
 
-$revision = @file_get_contents(APPDIR . DIRECTORY_SEPARATOR . 'RELEASE');
+$revision = @file_get_contents(APPDIR . DIRECTORY_SEPARATOR . 'revisions.log');
 
 
 echo '<h1>' . $config->application . '</h1>';
