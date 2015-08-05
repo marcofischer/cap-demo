f7fb323021c40e1257a78c4f9e6d690b4a259acf
Marco Fischer <mfischer@brainbits.net>
2015-08-05 17:32:03 +0200

    fixed

--- web/index.php
+++ web/index.php
@@ -8,6 +8,9 @@ $config = json_decode(file_get_contents(CONFIGDIR . DIRECTORY_SEPARATOR . 'confi
 
 $revision = @file_get_contents(APPDIR . DIRECTORY_SEPARATOR . 'REVISION');
 
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+
 
 echo '<h1>' . $config->application . '</h1>';
 echo '<pre>Revision: ' . $revision . PHP_EOL
