a3323214af82ef347415210497293c4d338395c5
Marco Fischer <mfischer@brainbits.net>
2015-08-05 16:56:47 +0200

    xxx

--- web/index.php
+++ web/index.php
@@ -1,2 +1,13 @@
-<?php 
+<?php
 
+define('APPDIR', dirname(dirname(__FILE__)));
+
+define('CONFIGDIR', APPDIR . DIRECTORY_SEPARATOR . 'config');
+
+$config = json_decode(file_get_contents(CONFIGDIR . DIRECTORY_SEPARATOR . 'config.json'));
+
+$revision = @file_get_contents(APPDIR . DIRECTORY_SEPARATOR . 'RELEASE');
+
+
+echo '<h1>' . $config->application . '</h1>';
+echo '<pre>Revision: ' . $revision . '</pre>';
